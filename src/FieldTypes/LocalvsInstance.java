cfb045f71312222b3d654fd67fbce90217b73e3b
osumnulu <sumnuluokan@gmail.com>
2022-07-24 21:40:54 +0300

    instance versus local example

---
+++ src/FieldTypes/LocalvsInstance.java
@@ -0,0 +1,34 @@
+package FieldTypes;
+
+public class LocalvsInstance {
+
+    public int a = 10;  //instance variable--belongst to the object created from this class
+
+    public void showDifference(){
+
+        int a = 5;     //local variable
+        System.out.println(a);
+    }
+
+
+
+    public static void main(String[] args) {
+        LocalvsInstance obj = new LocalvsInstance();  //default constructor even if we don't write one
+
+
+        System.out.println("****coming from the method : local---");
+
+        obj.showDifference();
+
+        System.out.println("****coming from instance variable****");
+        System.out.println(obj.a);
+
+
+        obj.a=1;
+
+        System.out.println(obj.a);
+
+
+    }
+
+}
